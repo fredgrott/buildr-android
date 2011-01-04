625ff2c30a10dbb09e0f563831d2ff202f188d2c
Carl Harroch <carl@novoda.com>
2011-01-04 10:23:09 +0000

    moving machine

--- spec/buildr/eclipse/eclipse_spec.rb
+++ spec/buildr/eclipse/eclipse_spec.rb
@@ -2,23 +2,21 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../../', 'spec_helpe
 
 describe Buildr::Eclipse::Android do
   it "should get the type of project from options" do    
-    # define 'root' do      
-    #   define 'foo', :android_type => :robolectric do
-    #   end
-    #   
-    #   define 'bar', :android_type => :instrumentation do
-    #   end
-    #   
-    #   define 'libfoo', :android_type => :library do
-    #   end
-    #   
-    #   define 'main', :android_type => :main do
-    #   end
-    # end
-    # project('root:foo').android_type.should eql(:robolectric)
-    # project('root:bar').android_type.should eql(:instrumentation)
-    # project('root:libfoo').android_type.should eql(:library)
-    # # project('root:foo').task('eclipse').invoke
-    1.should eql 1
+    define 'root' do      
+      define 'foo', :android_type => :robolectric do
+      end
+      
+      define 'bar', :android_type => :instrumentation do
+      end
+      
+      define 'libfoo', :android_type => :library do
+      end
+      
+      define 'main', :android_type => :main do
+      end
+    end
+    project('root:foo').android_type.should eql(:robolectric)
+    project('root:bar').android_type.should eql(:instrumentation)
+    project('root:libfoo').android_type.should eql(:library)    
   end
 end
--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -1,11 +1,14 @@
+require "rubygems"
+require "bundler/setup"
+
 unless defined?(SpecHelpers)
   module SandboxHook
-    def SandboxHook.included(spec_helpers)      
+    def SandboxHook.included(spec_helpers)
       spec = Gem::Specification.load(File.expand_path('../buildr-android.gemspec', File.dirname(__FILE__)))
       spec.dependencies.select {|dep| dep.type == :runtime }.each { |dep| gem dep.name, (dep.respond_to?(:requirement) ? dep.requirement.to_s : dep.version_requirements.to_s) }
       $LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
       require 'buildr-android'
     end
   end
-  require File.join(File.dirname(__FILE__), "../buildr/spec/spec_helpers.rb")
+  require File.join(File.dirname(__FILE__), "/../buildr/spec/spec_helpers.rb")
 end
