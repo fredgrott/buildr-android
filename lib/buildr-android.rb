c0caa4af3879e9466ea45758b4600b42fc0ad0bd
Carl-Gustaf Harroch <carl@novoda.com>
2011-01-08 15:08:12 +0000

    bump

--- lib/buildr-android.rb
+++ lib/buildr-android.rb
@@ -2,6 +2,8 @@ require 'buildr'
 require 'buildr/java'
 require 'buildr/android/version'
 require 'buildr/android/config'
+require 'buildr/android/layout'
 require 'buildr/android/project_extension'
 require 'buildr/android/test'
+
 require 'buildr/eclipse/android'
--- lib/buildr/android/test.rb
+++ lib/buildr/android/test.rb
@@ -1,5 +1,21 @@
-module Buildr
-  class Robolectric < TestFramework::Java
+module Buildr  
+  class Robolectric < Junit
+    
+    # Robolectric version number.
+    VERSION = '0.9.4'
+
+    class << self
+      def dependencies        
+        @dependencies ||= ["com.pivotallabs:robolectric:jar:#{version}"]
+      end
+    end
+    
+    def run(tests, dependencies)
+      # ensure android.jar is put last upon junit test runs - needed by robolectric
+      super.run(tests, dependencies.sort_by {|w| (w.include? 'android') ? 1 : -1 })
+    end
+    
+    task('clean') { }
   end
   
   class Instrumentation < TestFramework::Java
